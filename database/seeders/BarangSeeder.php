a6bbfc7af2db5fb5ae595a9fd598366ea9c8ae33
As'ad Muhaimin Iskandar <90304523+iska08@users.noreply.github.com>
2022-03-26 07:49:42 +0800

    Kuis 1

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -15,13 +15,60 @@ class BarangSeeder extends Seeder
     public function run()
     {
         DB::table('barangs')->insert([
-            'id' => '7',
+            'judul_buku' => 'Tetralogi Pulau Buru',
+            'pengarang' => 'Pramoedya Ananta Toer',
+            'penerbit' => 'Lentera Dipantara',
+            'tahun_terbit' => '1980',
+            'harga' => 500000,
+            'stok' => 100
+        ]);
+        DB::table('barangs')->insert([
+            'judul_buku' => 'Ziarah',
+            'pengarang' => 'Iwan Simatupang',
+            'penerbit' => 'Djambatan',
+            'tahun_terbit' => '1966',
+            'harga' => 30000,
+            'stok' => 100
+        ]);
+        DB::table('barangs')->insert([
+            'judul_buku' => 'Lelaki Harimau',
+            'pengarang' => 'Eka Kurniawan',
+            'penerbit' => 'Gramedia Pustaka Utama',
+            'tahun_terbit' => '2004',
+            'harga' => 55000,
+            'stok' => 100
+        ]);
+        DB::table('barangs')->insert([
+            'judul_buku' => 'Olengka',
+            'pengarang' => 'Budi Darma',
+            'penerbit' => 'Balai Pustaka',
+            'tahun_terbit' => '1983',
+            'harga' => 75000,
+            'stok' => 100
+        ]);
+        DB::table('barangs')->insert([
+            'judul_buku' => 'Ronggeng Dukuh Paruk',
+            'pengarang' => 'Ahmad Tohari',
+            'penerbit' => 'Gramedia Pustaka Utama',
+            'tahun_terbit' => '2003',
+            'harga' => 75000,
+            'stok' => 100
+        ]);
+        DB::table('barangs')->insert([
+            'judul_buku' => 'Cantik Itu Luka',
+            'pengarang' => 'Eka Kurniawan',
+            'penerbit' => 'Gramedia Pustaka Utama',
+            'tahun_terbit' => '2004',
+            'harga' => 125000,
+            'stok' => 100
+        ]);
+        DB::table('barangs')->insert([
             'judul_buku' => 'Atheis',
             'pengarang' => 'Achdiat K. Mihardja',
             'penerbit' => 'Balai Pustaka',
             'tahun_terbit' => '1950',
-            'harga' => '104000',
-            'stok' => '100'
+            'harga' => 104000,
+            'stok' => 100
         ]);
     }
 }
