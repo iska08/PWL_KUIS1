7db95cf758e83d3b7d8ad9352fcce9fe4cacc35c
As'ad Muhaimin Iskandar <90304523+iska08@users.noreply.github.com>
2022-03-26 07:45:58 +0800

    Kuis 1

--- app/Models/Pelanggan.php
+++ app/Models/Pelanggan.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pelanggan extends Model
 {
-    use HasFactory;
+    protected $table = 'my_pelanggans';
+    protected $primaryKey = 'my_id';
+    protected $keyType = 'string';
+    public $incrementing = false;
 }
